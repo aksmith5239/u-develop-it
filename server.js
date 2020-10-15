719db5de8204983400d6f252bbea5760b1d39aac
aksmith5239 <aklobby@gmail.com>
2020-10-15 10:01:07 -0600

    updated modularization

--- server.js
+++ server.js
@@ -13,55 +13,6 @@ const apiRoutes = require('./routes/apiRoutes');
 app.use('/api', apiRoutes);
 
 // const htmlRoutes = require('./routes/htmlRoutes');
-//routes for all parties
-app.get('/api/parties', (req, res) => {
-  const sql = `SELECT * FROM parties`;
-  const params = [];
-  db.all(sql, params, (err, rows) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
-
-    res.json({
-      message: 'success',
-      data: rows
-    });
-  });
-});
-
-// get by party id
-app.get('/api/party/:id', (req, res) => {
-  const sql = `SELECT * FROM parties WHERE id = ?`;
-  const params = [req.params.id];
-  db.get(sql, params, (err, row) => {
-    if(err) {
-      res.status(400).json({error: err.message});
-      return;
-    }
-
-    res.json({
-      message: 'success',
-      data: row
-    });
-  });
-});
-
-//delete party
-app.delete('/api/party/:id', (req, res) => {
-  const sql = `DELETE FROM parties WHERE id = ?`;
-  const params = [req.params.id];
-  db.run(sql, params, function(err, result) {
-    if(err) {
-      res.status(400).json({ error: err.message});
-      return;
-    }
-
-    res.json({
-      message: 'successfully deleted', changes: this.changes
-    });
-  });
-});
 
 // catch all route
 app.use((req,res) => {
